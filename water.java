6a8c533e627ecf5d2e4a2898d3d07193949aabff
Nikita-Aswal <aswalnikita246@gmail.com>
2025-08-30 22:13:17 +0530

    Add files via upload

---
+++ water.java
@@ -0,0 +1,23 @@
+public class water {
+
+ 
+    public int maxArea(int[] height) {
+        int l=0;
+        int r= height.length-1;
+        int maxArea=0;
+        while(l<r){
+            int area= Math.min(height[l],height[r])*(r-l);
+            maxArea= Math.max(area,maxArea);
+            if(height[l]<height[r]){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return maxArea;
+
+    }
+}
+   
+
